4ea5651a585b69ed9a012d8f2e924664380fcd59
taylorpeer <t.peer@cortical.io>
2014-12-21 23:37:50 -0500

    Reverted to working example code

--- exercise-1/start.bat
+++ exercise-1/start.bat
@@ -1,1 +1,1 @@
-java -cp target/lib/jade-4.3.3.jar;target/classes jade.Boot -gui
+java -cp target/lib/jade-4.3.3.jar;target/classes jade.Boot -gui -agents "p1:at.ac.tuwien.ifs.sos.PrisonerAgent(at.ac.tuwien.ifs.sos.strategies.RandomStrategy);p2:at.ac.tuwien.ifs.sos.PrisonerAgent(at.ac.tuwien.ifs.sos.strategies.RandomStrategy);pm:at.ac.tuwien.ifs.sos.GamemasterAgent(p1, p2, 500)"
